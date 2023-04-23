51f71d758239ff85f056a91f8254107694bae9f7
mhenriette <mhenriette00@gnmail.com>
2023-04-23 12:13:15 +0200

    feat: design van details

--- src/pages/VansDetails.jsx
+++ src/pages/VansDetails.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Heading2 } from "../components/ui/Typography";
+import { Heading1, Heading2, Heading4 } from "../components/ui/Typography";
+import Button from "../components/ui/Button"
 
 const VansDetails = () => {
     const [details, setDetails] = useState("")
@@ -12,8 +13,19 @@ const VansDetails = () => {
 
     }, [])
     console.log(details)
-    return <div>
-        {details ? <h1>{details.id}</h1> : <div className="w-full flex justify-center items-center min-h-screen">
+    return <div className="w-full flex justify-center">
+        {details ? <div className="md:w-1/2 px-10 md:px-0">
+            <div className="w-full h-1/2 rounded-md overflow-hidden my-5">
+                <img src={details.imageUrl} className="w-full h-full" />
+            </div>
+            <div className="flex flex-col items-start gap-y-5">
+                <Button className="bg-orange-600 inline-block text-white">{details.type}</Button>
+                <Heading1>{details.name}</Heading1>
+                <div className="flex items-center text-[30px]"><Heading2>${details.price}</Heading2>/day</div>
+                <Heading4>{details.description}</Heading4>
+            </div>
+            <Button className="bg-orange-600 text-white my-3">Book this van</Button>
+        </div> : <div className="w-full flex justify-center items-center min-h-screen">
             <Heading2>Loading...</Heading2>
 
         </div>}
