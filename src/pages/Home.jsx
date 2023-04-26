d8839ef156fbf7e1d8cc6916fd2aaf6db928dd15
mhenriette <mhenriette00@gnmail.com>
2023-04-26 17:08:12 +0200

    fix: add navigation link to the buttons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import Button from "../components/ui/Button";
+import { Link } from "react-router-dom";
 
 import { Heading1, Heading4 } from "../components/ui/Typography";
 
@@ -8,12 +9,14 @@ const Home = () => {
             <main className="flex-1 flex items-center justify-center p-4 bg-black bg-opacity-40">
                 <div className="py-16 mx-auto text-center items-center flex flex-col">
                     <Heading1>You got the travel plans, we got the travel vans.</Heading1>
-                  <Heading4 className="pt-6 max-w-4xl">
-                      Add adventure to your life by joining the #vanlife movement. Rent
-                      the perfect van to make your perfect road trip.
-                  </Heading4>
-                  <Button className="bg-[#FF8C38] w-1/2 mt-14 py-4">
-                      Find your van
+                    <Heading4 className="pt-6 max-w-4xl">
+                        Add adventure to your life by joining the #vanlife movement. Rent
+                        the perfect van to make your perfect road trip.
+                    </Heading4>
+                    <Button className="bg-[#FF8C38] w-1/2 py-0 px-0 mt-14 ">
+                        <Link to="/vans" className="block py-4">
+                            Find your van
+                        </Link>
                   </Button>
               </div>
           </main>
